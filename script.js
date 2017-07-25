fe5fc887a5e4871b579575bb726d15a3cc2e7eb1
rrgayhart <rrgayhart@gmail.com>
2017-07-25 10:52:53 -0600

    Submit button appends values to the grocery list

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var $submitButton = $('.grocery-list-form--submit');
+var $itemInput = $('.grocery-list-form--item');
+var $quantityInput = $('.grocery-list-form--quantity');
+var $list = $('.grocery-list');
 
+$submitButton.on('click', function(e) {
+  e.preventDefault();
+  var item = $itemInput.val();
+  var quantity = $quantityInput.val();
+
+  $list.append('<li>Item: ' + item + ' Quantity: ' + quantity + '</li>')
+});
